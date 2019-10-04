3c02a13c3700d35a27d49b11136ff468220e42cf
Ian Andrewson <ianandrewson@gmail.com>
2019-10-04 09:58:35 -0700

    refactored a whole bunch of code

--- aboutme.js
+++ aboutme.js
@@ -1,4 +1,4 @@
-import { isCorrect } from './functions.js';
+import { isCorrect, sanitizedUserAnswerFunction } from './functions.js';
 
 const myButton = document.getElementById('action-button');
 
@@ -7,36 +7,5 @@ myButton.onclick = function() {
         
     const userAnswer1 = prompt('is her fav color coffee?');
     const sanitizedUserAnswer = userAnswer1.toLowerCase();
-    // const userIsCorrect = isCorrect(sanitizedUserAnswer);
-
-    if (isCorrect(sanitizedUserAnswer)) { 
-        alert('hell yeah');
-    } else {
-        alert ('nah');
-    }
-        // console.log(myConfirmation); 
-
-    const userAnswer2 = prompt('has she left the united states ever in her life?');
-    // const sanitizedUserAnswer = userAnswer.toLowerCase();
-    // const userIsCorrect = isCorrect(sanitizedUserAnswer);
-
-    if (userAnswer2 === 'yes'){
-        alert ('you are correct');
-    } else {
-        alert ('nahhh b');
-    }
-    
-    const userAnswer3 = prompt('does the author have a big big dog?');
-    // const sanitizedUserAnswer = userAnswer.toLowerCase();
-    // const userIsCorrect = isCorrect(sanitizedUserAnswer);
-
-    if (userAnswer3 === 'no'){
-        alert ('congratulations on your successful knowledge absortion');
-    } else {
-        alert ('nahhh b');
-    }
-
+    sanitizedUserAnswerFunction(sanitizedUserAnswer);
 };
-
-// 
-// 
--- functions.js
+++ functions.js
@@ -5,4 +5,28 @@ export const isCorrect = (userAnswer) => {
     else { 
         return false; 
     }
+}
+
+export const questionOneFunction = (sanitizedUserAnswer) => {
+    if (isCorrect(sanitizedUserAnswer)) { 
+        alert('hell yeah');
+    } else {
+        alert ('nah');
+    }   
+};
+export const questionThreeFunction = (sanitizedUserAnswer) => {
+    if (!isCorrect(sanitizedUserAnswer)) { 
+        alert('hell yeah');
+    } else {
+        alert ('nah');
+    }   
+};
+
+export const sanitizedUserAnswerFunction = (sanitizedUserAnswer) => {
+    questionOneFunction(sanitizedUserAnswer);
+    const userAnswer2 = prompt('has she left the united states ever in her life?');
+    questionOneFunction(userAnswer2);
+    const userAnswer3 = prompt('does the author have a big big dog?');
+    questionThreeFunction(userAnswer3);
+    sanitizedUserAnswer
 };
